d7269dcb76ca2f663d6a5a858e5a947ec58f6504
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2025-01-22 02:46:54 +0900

    add test for gradle.ts

--- __tests__/gradle.test.ts
+++ __tests__/gradle.test.ts
@@ -1,22 +1,72 @@
 import {
+  execDependenciesTask,
   execGradleProjects,
   filterGradleProjects,
+  generateDependenciesFiles,
   getDependenciesTasks,
   getProjectFromTask,
   parseGradleProjects
 } from '../src/gradle'
 import { jest } from '@jest/globals'
 import * as exec from '@actions/exec'
+import * as io from '@actions/io'
+import path from 'node:path'
+import fs from 'fs'
 
 describe('gradle.ts', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
+  describe('generateDependenciesFiles', () => {
+    const getExecOutput = jest.spyOn(exec, 'getExecOutput')
+    const mkdirP = jest.spyOn(io, 'mkdirP')
+    const writeFileSync = jest.spyOn(fs, 'writeFileSync')
+
+    it('test', async () => {
+      getExecOutput.mockResolvedValueOnce({
+        stdout: `+--- Project ':hoge'\n+--- Project ':fuga'`
+      } as exec.ExecOutput)
+      mkdirP.mockResolvedValue()
+      getExecOutput.mockResolvedValue({
+        exitCode: 0,
+        stdout: 'stdout'
+      } as exec.ExecOutput)
+      writeFileSync.mockImplementation(() => {})
+
+      await generateDependenciesFiles(
+        {
+          includeProjectRegex: '',
+          excludeProjectRegex: '',
+          includeRootProject: false,
+          configurations: 'compileClasspath, runtimeClasspath'
+        },
+        '/temp'
+      )
+
+      expect(writeFileSync).toHaveBeenCalledWith(
+        path.join('/temp', ':hoge', `compileClasspath.txt`),
+        'stdout'
+      )
+      expect(writeFileSync).toHaveBeenCalledWith(
+        path.join('/temp', ':hoge', `runtimeClasspath.txt`),
+        'stdout'
+      )
+      expect(writeFileSync).toHaveBeenCalledWith(
+        path.join('/temp', ':fuga', `compileClasspath.txt`),
+        'stdout'
+      )
+      expect(writeFileSync).toHaveBeenCalledWith(
+        path.join('/temp', ':fuga', `runtimeClasspath.txt`),
+        'stdout'
+      )
+    })
+  })
+
   describe('execGradleProjects', () => {
     const getExecOutput = jest.spyOn(exec, 'getExecOutput')
 
-    it('execGradleProjects', async () => {
+    it('test', async () => {
       getExecOutput.mockResolvedValueOnce({
         stdout: 'stdout'
       } as exec.ExecOutput)
@@ -104,6 +154,53 @@ Root project 'root'
     it('includeRootProject false', () => {
       const result = getDependenciesTasks([':hoge', ':fuga'], false)
       expect(result).toEqual([':hoge:dependencies', ':fuga:dependencies'])
+    })
+  })
+
+  describe('execDependenciesTask', () => {
+    const mkdirP = jest.spyOn(io, 'mkdirP')
+    const getExecOutput = jest.spyOn(exec, 'getExecOutput')
+    const writeFileSync = jest.spyOn(fs, 'writeFileSync')
+
+    it('test', async () => {
+      mkdirP.mockResolvedValueOnce()
+      getExecOutput.mockResolvedValueOnce({
+        stdout: 'stdout1',
+        exitCode: 0
+      } as exec.ExecOutput)
+      getExecOutput.mockResolvedValueOnce({
+        exitCode: 1
+      } as exec.ExecOutput)
+      writeFileSync.mockImplementation(() => {})
+
+      await execDependenciesTask(
+        ':hoge:dependencies',
+        ['compileClasspath', 'runtimeClasspath'],
+        '/temp'
+      )
+
+      expect(mkdirP).toHaveBeenCalledWith(path.join('/temp', ':hoge'))
+      expect(getExecOutput).toHaveBeenCalledWith(
+        './gradlew',
+        [':hoge:dependencies', '--configuration', 'compileClasspath'],
+        {
+          ignoreReturnCode: true,
+          silent: true
+        }
+      )
+      expect(getExecOutput).toHaveBeenCalledWith(
+        './gradlew',
+        [':hoge:dependencies', '--configuration', 'runtimeClasspath'],
+        {
+          ignoreReturnCode: true,
+          silent: true
+        }
+      )
+      expect(writeFileSync).toHaveBeenCalledTimes(1)
+      expect(writeFileSync).toHaveBeenCalledWith(
+        path.join('/temp', ':hoge', `compileClasspath.txt`),
+        'stdout1'
+      )
     })
   })
 
