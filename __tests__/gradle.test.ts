2cd0453bbd298f5f755bb44bdb2e6101cf421aef
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2025-01-23 03:06:45 +0900

    add test

--- __tests__/gradle.test.ts
+++ __tests__/gradle.test.ts
@@ -1,13 +1,23 @@
 import { jest } from '@jest/globals'
+import * as exec from '@actions/exec'
+import { generateDependenciesFiles } from '../src/gradle'
 
 describe('gradle.ts', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  describe('hoge', () => {
-    it('hoge', () => {
-      expect(1).toEqual(1)
+  describe('generateDependenciesFiles', () => {
+    const getExecOutput = jest.spyOn(exec, 'getExecOutput')
+
+    it('test', async () => {
+      getExecOutput.mockResolvedValueOnce({} as exec.ExecOutput)
+      await generateDependenciesFiles('configuration', 'cwd')
+      expect(getExecOutput).toHaveBeenCalledWith(
+        './gradlew',
+        ['clean', 'dependencyReport', '--configuration', 'configuration'],
+        { cwd: 'cwd' }
+      )
     })
   })
 })
