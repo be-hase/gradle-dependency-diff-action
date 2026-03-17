75a96a6f91883beec9be67270676c58b6cb4c050
R4nSC <52380218+R4nSC@users.noreply.github.com>
2026-03-17 10:36:00 +0900

    Fix submodule-aware clone command (#77)
    (cherry picked from commit b371bc71a47779fea4ca94a39a3c968137057d6b)

--- dist/index.js
+++ dist/index.js
@@ -40221,10 +40221,23 @@ function getGitUrl(token) {
 }
 // export for testing
 async function cloneBaseRepository(gitUrl, baseRepoDir) {
+    const repoUrl = new URL(gitUrl);
+    const auth = repoUrl.password.length
+        ? `${repoUrl.username}:${repoUrl.password}@`
+        : `${repoUrl.username}@`;
+    const authenticatedOriginWithSlash = `${repoUrl.protocol}//${auth}${repoUrl.host}/`;
+    const rewriteScpLikeSshUrl = `url.${authenticatedOriginWithSlash}.insteadOf=git@${repoUrl.host}:`;
+    const rewriteSshUrl = `url.${authenticatedOriginWithSlash}.insteadOf=ssh://git@${repoUrl.host}/`;
     await execExports.exec('git', [
+        '-c',
+        rewriteScpLikeSshUrl,
+        '-c',
+        rewriteSshUrl,
         'clone',
         '--depth',
         '1',
+        '--recurse-submodules',
+        '--shallow-submodules',
         '-b',
         githubExports.context.payload.pull_request?.base.ref,
         gitUrl,
